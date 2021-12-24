2fceb7066d7528f8b5bc923df40e3fcc7dc26dfa
ValeryShestakovv <medicmtt@gmail.com>
2021-12-24 19:16:30 +0600

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_lab/pages/first_page_home.dart';
+import 'package:flutter_lab/pages/front_page.dart';
 
 void main() {
   runApp(const Main());
 }
-
 
 class Main extends StatelessWidget {
   const Main({Key? key}) : super(key: key);
